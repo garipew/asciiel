8ae290f68cf3f2740b975452e2ab7fd8209345c4
Gabriel Sá <gabriel10043@live.com>
2024-06-20 14:32:25 -0300

    add arg treatment

--- main.lua
+++ main.lua
@@ -1,3 +1,42 @@
+function readArgs()
+	
+	local w = false
+	local h = false
+	local invert = false
+	local complete = false
+
+	local width = 0
+	local height = 0
+	
+	for i=1,#arg do
+		local num = tonumber(arg[i])
+		if num ~= nil then
+			if not w then
+				width = num
+				w = true
+			elseif not h then
+				height = num
+				h = true
+			end
+			goto continue
+		end
+
+		if arg[i] == "true" then
+			invert = true
+		elseif arg[i] == "false" then
+			invert = false
+		elseif arg[i] == "complete" then
+			complete = true
+		end	
+				
+		::continue::
+	end
+	
+	return width,height,invert,complete	
+
+end
+
+
 love.window.close()
 
 local imagem = love.image.newImageData(arg[2])
@@ -7,21 +46,18 @@ local ratio = width/height
 local width_scale = 200/width
 local height_scale = width_scale / ratio
 
-local invert = false
-if #arg >= 3 then
-	if arg[3] == "true" then invert = true end
-end
-
-if #arg >= 4 then
-	width_scale = arg[4]/width
+local w,h,invert,complete = readArgs()
+if w ~= 0 then
+	width_scale = w/width
 	height_scale = width_scale / ratio
 end
-
-if #arg >= 5 then
-	height_scale = arg[5]/height
+if h ~= 0 then
+	height_scale = h/height
 end
 
-local ascii = "@%#*+=-:. " 
+
+local ascii = " .:-=+*#%@"
+if complete then ascii = " .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$" end
 local range = 255/string.len(ascii)
 
 local str = "" 
